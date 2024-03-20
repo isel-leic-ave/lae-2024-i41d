868712fbf60a455c0ddb7407acebf6fd6e45b084
Miguel Gamboa <miguelgamboa@outlook.com>
2024-03-20 17:07:38 +0000

    lesson 11

---
+++ lesson11-naivemapper/src/test/kotlin/pt/isel/ArtistSpotify.kt
@@ -0,0 +1,3 @@
+package pt.isel
+
+class ArtistSpotify(val name: String, val country: String)
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,3 +7,4 @@ rootProject.name = "lae-2024-i41d"
 include("lesson09-reflect")
 include("lesson09-TPC")
 include("lesson10-logger")
+include("lesson11-naivemapper")
